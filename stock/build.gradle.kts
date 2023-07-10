d56b0285f1f0677fd1959d4b2edcb6c4a68cbb35
Rafaela Peres <62906605+rsanPeres@users.noreply.github.com>
2023-07-10 13:56:54 -0300

    Add eureka service application

--- stock/build.gradle.kts
+++ stock/build.gradle.kts
@@ -26,7 +26,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.2")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
